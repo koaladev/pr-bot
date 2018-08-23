fada3f2ca6c2283f92c6157a9e681656dca44a90
Kristo Jrisson <k.jurisson@koala.com>
2018-08-23 17:51:33 +1000

    Update github package

--- src/controllers/github-controller.js
+++ src/controllers/github-controller.js
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-const GitHubApi = require('github');
+const GitHubApi = require('@octokit/rest');
 const logHelper = require('../utils/log-helper');
 
 class GithubController {
